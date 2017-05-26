287476be51cb8ef80c5cd4c7a9492e88952c4c0f
Ian Leatherbury <ian.leatherbury@gmail.com>
2017-05-26 16:55:29 -0700

    Get contacts and populate tableview
    Populate ChooseContactViewController with your contacts. Also updates permissions to access a user's contacts

--- Confetti/ChooseContactViewController.swift
+++ Confetti/ChooseContactViewController.swift
@@ -8,18 +8,48 @@
 
 import Foundation
 import UIKit
+import Contacts
+import ContactsUI
 
 class ChooseContactViewController : UITableViewController {
-    var objects = ["Stu", "David", "Ian"]
+    
+    var contacts = [CNContact]()
+    let contactStore = CNContactStore()
+    
+    override func viewDidLoad() {
+        getContacts()
+    }
+    
+    func getContacts() {
+        let keys = [CNContactFormatter.descriptorForRequiredKeys(for: .fullName)]
+        let request = CNContactFetchRequest(keysToFetch: keys)
+        
+        do {
+            try self.contactStore.enumerateContacts(with: request) {
+                (contact, stop) in
+                // Array containing all unified contacts from everywhere
+                self.contacts.append(contact)
+            }
+        }
+        catch {
+            print("unable to fetch contacts")
+        }
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-            cell.textLabel?.text = objects[indexPath.item]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath as IndexPath)
+        
+        let contact = self.contacts[indexPath.row]
+        let formatter = CNContactFormatter()
+        
+        cell.textLabel?.text = formatter.string(from: contact)
+        cell.detailTextLabel?.text = contact.emailAddresses.first?.value as String?
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return contacts.count
     }
 }
 
