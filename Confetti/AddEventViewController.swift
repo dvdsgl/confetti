08d33dba74d06dd3fb2eec02b8e011bdbe6ec179
Ian Leatherbury <ian.leatherbury@gmail.com>
2017-06-01 06:14:16 -0700

    Refactor some stuff
    Now using an unwind segue in AddEventController, and cleaned up some animation code.

--- Confetti/AddEventViewController.swift
+++ Confetti/AddEventViewController.swift
@@ -5,23 +5,17 @@ import ConfettiKit
 
 class AddEventViewController : UIViewController {
     
-    @IBAction func cancelAddEvent(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-    }
-    
     @IBOutlet var translucentOverlay: UIView!
     
     @IBOutlet var buttonsDrawer: UIView!
     
     override func viewDidAppear(_ animated: Bool) {
         
-        buttonsDrawer.superview?.bringSubview(toFront: buttonsDrawer)
+        view.bringSubview(toFront: buttonsDrawer)
         
         UIView.animate(withDuration: 1.0, animations: { self.translucentOverlay.backgroundColor = UIColor.white }, completion: nil)
         
-        UIView.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 15, options: [], animations: {
-            self.buttonsDrawer?.center = CGPoint(x: (self.buttonsDrawer?.center.x)!, y: self.buttonsDrawer.frame.height)
-        }, completion:nil)
+        UIView.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 15, options: [], animations: { self.buttonsDrawer?.center = CGPoint(x: (self.buttonsDrawer?.center.x)!, y: self.buttonsDrawer.frame.height) }, completion:nil)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
