a18ce770700f1cc84b9c64605de20ad7b1a3fbd1
Ian Leatherbury <ian.leatherbury@gmail.com>
2017-06-07 20:32:09 -0700

    Add outlets and label to pillview

--- Confetti/CountdownPillView.swift
+++ Confetti/CountdownPillView.swift
@@ -1,11 +1,20 @@
 import Foundation
 import UIKit
 
+import ConfettiKit
+
 @IBDesignable
 class CountdownPillView : UIView {
+    
+    @IBOutlet weak var pillTitleLabel: UILabel!
+    @IBOutlet weak var pillShortDateLabel: UILabel!
     
     override func layoutSubviews() {
         super.layoutSubviews()
         layer.cornerRadius = self.bounds.height / 2
+    }
+    
+    public func setEvent(_ event: EventViewModel) {
+        pillTitleLabel.text = event.title
     }
 }
