7098bc163b434920344925c50e5cd286ea6e51b4
Ian Leatherbury <ian.leatherbury@gmail.com>
2017-06-07 19:57:12 -0700

    Merge branch 'master' of https://github.com/dvdsgl/confetti-swift

---
+++ Confetti/AppDelegate+Notifications.swift
@@ -0,0 +1,92 @@
+import Foundation
+import UserNotifications
+
+import ConfettiKit
+
+extension AppDelegate {
+    
+    func notifications(for event: Event) -> [UNNotificationRequest] {
+        let viewModel = EventViewModel.fromEvent(event)
+        let baseDate = viewModel.nextOccurrence
+        
+        let calendar = Calendar.current
+        
+        return viewModel.notifications.map { spec in
+            let content = UNMutableNotificationContent()
+            content.title = spec.title
+            content.body = spec.message
+            content.sound = UNNotificationSound.default()
+            
+            let date = calendar.date(byAdding: .day, value: -spec.daysBefore, to: baseDate)!
+            let trigger = UNCalendarNotificationTrigger(
+                dateMatching: DateComponents(
+                    month: calendar.component(.month, from: date),
+                    day: calendar.component(.day, from: date),
+                    hour: 9
+                ),
+                repeats: false
+            )
+            
+            let identifier = (viewModel.event.key ?? "") + spec.id
+            let request = UNNotificationRequest(
+                identifier: identifier,
+                content: content,
+                trigger: trigger
+            )
+            
+            return request
+        }
+    }
+    
+    func scheduleNotifications(for events: [Event]) {
+        let center = UNUserNotificationCenter.current()
+        
+        let notifications = events.flatMap { self.notifications(for: $0) }
+        for notification in notifications {
+            center.add(notification) { error in
+                if error != nil {
+                    // Handle any errors
+                }
+            }
+        }
+    }
+    
+    func scheduleSampleNotification(withDelay: TimeInterval = 0) {
+        guard let events = UserViewModel.current.events else { return }
+        
+        let requests = events.flatMap { notifications(for: $0) }
+        let request = requests[Int(arc4random_uniform(UInt32(requests.count)))]
+        
+        let calendar = Calendar.current
+        let date = Date(timeIntervalSinceNow: 0.2 + withDelay)
+        let dateMatch = calendar.dateComponents([.hour, .minute, .second], from: date)
+        let newRequest = UNNotificationRequest(
+            identifier: "sample",
+            content: request.content,
+            trigger: UNCalendarNotificationTrigger(dateMatching: dateMatch, repeats: false)
+        )
+        
+        UNUserNotificationCenter.current().add(newRequest)
+    }
+    
+    // Handle foreground notifications
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent: UNNotification,
+                                withCompletionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        withCompletionHandler(.alert)
+    }
+    
+    // TODO: Handle what happens when user swipes on notification
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        if response.actionIdentifier == UNNotificationDismissActionIdentifier {
+            // The user dismissed the notification without taking action
+        }
+        else if response.actionIdentifier == UNNotificationDefaultActionIdentifier {
+            // The user launched the app
+        }
+        
+        // Else handle any custom actions. . .
+    }
+}
--- Confetti/ProfileViewController.swift
+++ Confetti/ProfileViewController.swift
@@ -1,71 +1,57 @@
-//
-//  ProfileViewController.swift
-//  Confetti
-//
-//  Created by Ian Leatherbury on 5/22/17.
-//  Copyright © 2017 confetti. All rights reserved.
-//
-
 import Foundation
 import UIKit
+
 import ConfettiKit
+
 import FRStretchImageView
 import FirebaseAuth
 import Firebase
 
 import MobileCenterCrashes
 
-import UserNotifications
-
 class ProfileViewController : UITableViewController {
     
     @IBOutlet var profileTableView: UITableView!
-
     @IBOutlet weak var profileImage: FRStretchImageView!
     
     var handle: AuthStateDidChangeListenerHandle?
     
-    fileprivate var source = [
-        "Name",
-        "Crash the app!",
-        "Logout",
-        "Test Notification"
-    ]
+    enum Action: String {
+        case name = "Name"
+        case logout = "Logout"
+        case crash = "Crash the app!"
+        case testNotification = "Send test notification"
+    }
+    
+    let source: [Action] = [.name, .logout, .crash, .testNotification]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Your Profile"
-        
-        // Setting FRStretchImageView
         profileImage.stretchHeightWhenPulledBy(scrollView: tableView)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // [START auth_listener]
-        handle = Auth.auth().addStateDidChangeListener { (auth, user) in            
+        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
             // Set user's facebook photo as the hero image
             if let facebookUserId = Auth.auth().currentUser?.providerData.first?.uid {
-                let photoUrl = URL(string: "https://graph.facebook.com/" + facebookUserId + "/picture?height=500")
+                let photoUrl = URL(string: "https://graph.facebook.com/\(facebookUserId)/picture?height=500")
                 self.profileImage.sd_setImage(with: photoUrl)
             } else {
                 self.profileImage.image = #imageLiteral(resourceName: "stu")
             }
         }
-        // [END auth_listener]
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        // [START remove_auth_listener]
         Auth.auth().removeStateDidChangeListener(handle!)
-        // [END remove_auth_listener]
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) 
-        cell.textLabel?.text = source[indexPath.item]
+        cell.textLabel?.text = source[indexPath.item].rawValue
         return cell
     }
     
@@ -75,14 +61,14 @@ class ProfileViewController : UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         switch source[indexPath.row] {
-        case "Logout":
+        case .logout:
             logOut()
             return
-        case "Crash the app!":
+        case .crash:
             MSCrashes.generateTestCrash()
             return
-        case "Test Notification":
-            testNotification()
+        case .testNotification:
+            AppDelegate.shared.scheduleSampleNotification()
             return
         default:
             return
@@ -93,23 +79,4 @@ class ProfileViewController : UITableViewController {
         try! Auth.auth().signOut()
         performSegue(withIdentifier: "unwindToLogin", sender: self)
     }
-    
-    // Sends test notification 5 seconds after tapping
-    func testNotification() {
-        // Set up content
-        let content = UNMutableNotificationContent()
-        content.title = NSString.localizedUserNotificationString(forKey: "Hello!", arguments: nil)
-        content.body = NSString.localizedUserNotificationString(forKey: "Hello_message_body", arguments: nil)
-        content.sound = UNNotificationSound.default()
-        
-        // Deliver the notification in five seconds.
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-        let request = UNNotificationRequest(identifier: "FiveSecond", content: content, trigger: trigger) // Schedule the notification.
-        let center = UNUserNotificationCenter.current()
-        center.add(request) { (error : Error?) in
-            if error != nil {
-                // Handle any errors
-            }
-        }
-    }    
 }
