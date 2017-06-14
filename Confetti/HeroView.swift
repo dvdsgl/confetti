91a33b4a814d6fefb2cdffca313d8c15e5cab320
Ian Leatherbury <ian.leatherbury@gmail.com>
2017-06-13 20:12:02 -0700

    Add init methods and make IBDesignable

--- Confetti/HeroView.swift
+++ Confetti/HeroView.swift
@@ -2,6 +2,19 @@ import Foundation
 import UIKit
 import ConfettiKit
 
+@IBDesignable
 class HeroView: UIView {
-
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        
+    }
 }
