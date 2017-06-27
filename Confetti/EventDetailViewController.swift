50ed71cda72c3029db106e0a4837180cf419e179
Ian Leatherbury <ian.leatherbury@gmail.com>
2017-06-26 18:20:42 -0700

    Restore previous EventDetail TableView

--- Confetti/EventDetailViewController.swift
+++ Confetti/EventDetailViewController.swift
@@ -3,31 +3,31 @@ import Foundation
 
 import ConfettiKit
 
-class EventDetailViewController : UIViewController, UIImagePickerControllerDelegate,
-    UINavigationControllerDelegate, UIScrollViewDelegate {
+class EventDetailViewController : UITableViewController,
+    UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate,
+HeroStretchable {
     
     @IBOutlet weak var heroView: HeroView!
-    @IBOutlet var heroViewHeightConstraint: NSLayoutConstraint!
     
-    var event: EventViewModel!
-    
-    @IBOutlet var scrollView: UIScrollView!
+    var event: EventViewModel! {
+        didSet {
+            updateDisplay()
+        }
+    }
     
     enum Action: String {
+        case empty
         case call = "Call"
         case message = "Message"
         case faceTime = "FaceTime"
     }
     
-    let actions: [Action] = [.call, .message, .faceTime]
+    let actions: [Action] = [.empty, .call, .message, .faceTime]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        scrollView.delegate = self
-        scrollView.showsVerticalScrollIndicator = false
-        
-        heroViewHeightConstraint.constant = UIScreen.main.bounds.height / 2
+        setupStretchyHero()
         
         styleTransparentNavigationBar()
         updateDisplay()
@@ -37,8 +37,40 @@ class EventDetailViewController : UIViewController, UIImagePickerControllerDeleg
         heroView?.event = event
     }
     
-    func scrollViewDidScroll(_ scrollView: UIScrollView){
-        heroViewHeightConstraint.constant = -self.scrollView.contentOffset.y / 2
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        updateStretchyHero()
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let action = actions[indexPath.item]
+        
+        switch action {
+        case .empty:
+            let cell = UITableViewCell()
+            cell.backgroundColor = UIColor.lightGray
+            return cell
+        default:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "action", for: indexPath)
+            cell.textLabel?.text = action.rawValue
+            return cell
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.row {
+        case 0:
+            return 30
+        default:
+            return 50
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return actions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
     func pickPhoto() {
@@ -57,6 +89,7 @@ class EventDetailViewController : UIViewController, UIImagePickerControllerDeleg
         }
     }
     
+    
     @IBAction func displayActionSheet(_ sender: Any) {
         let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         
@@ -71,16 +104,7 @@ class EventDetailViewController : UIViewController, UIImagePickerControllerDeleg
         }))
         
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-            
+        
         self.present(alert, animated: true)
-    }
-    
-    @IBAction func callButtonAction(_ sender: Any) {
-    }
-    
-    @IBAction func messageButtonAction(_ sender: Any) {
-    }
-    
-    @IBAction func faceTimeButtonAction(_ sender: Any) {
     }
 }
