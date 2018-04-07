487e5faa7fca913f323b71bebd0b658d652220fc
David Siegel <dasiege@microsoft.com>
2018-04-07 11:02:42 -0700

    iPhone X

--- Confetti/CustomTabBarController.swift
+++ Confetti/CustomTabBarController.swift
@@ -17,7 +17,7 @@ class CustomTabBarController: UITabBarController {
         
         view.addConstraints([
             NSLayoutConstraint(item: addEventButton, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0),
-            NSLayoutConstraint(item: addEventButton, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: -4),
+            NSLayoutConstraint(item: addEventButton, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottomMargin, multiplier: 1, constant: 0),
             NSLayoutConstraint(item: addEventButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: size),
             NSLayoutConstraint(item: addEventButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: size),
             ])
